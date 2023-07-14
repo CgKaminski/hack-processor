efd833bd79f91e51a67abef85339dcee51620188
CgKaminski <cameron.g.kaminski@gmail.com>
2023-07-13 17:51:22 -0700

    Updated cpu module to include clk

--- src/cpu/cpu.sv
+++ src/cpu/cpu.sv
@@ -21,42 +21,44 @@
  */
 
 module cpu(input logic [15:0] inM, instruction,
-           input logic reset,
+           input logic reset, clk,
            output logic [15:0] outM,
            output logic [14:0] addressM, pc,
            output logic writeM);
 
-  // CPU instruction decoding
-  // if (instruction[15] == 0) then A-instruction
-  // else C-instruction
-  assign a_instruction = (instruction[15] == 1) ? aluOut: instruction;
+  always_ff @(posedge clk) begin
+    // CPU instruction decoding
+    // if (instruction[15] == 0) then A-instruction
+    // else C-instruction
+    assign a_instruction = (instruction[15] == 1) ? aluOut: instruction;
+  
+    // A-Register
+    assign a_load = (instruction[15] == 0) ? 1'b1: 1'b0;
+    ARegister #(16) a_register(.in(a_instruction), .load(a_load), .out(a_out));
+    assign aluA = (instruction[12] == 0) ? a_out: inM;
+  
+    // D-Register
+    DRegister #(16) d_register(.in(aluOut), .load(instruction[4]), .out(d_out));
+  
+    // ALU and output 
+    ALU alu(.x(aluA), .y(d_out), .zx(instruction[11]), .nx(instruction[10]), 
+            .zy(instruction[9]), .ny(instruction[8]), .f(instruction[7]), 
+            .no(instruction[6]), .out(aluOut), .zr(zr), .ng(ng));
+  
+    // Memory output
+    assign writeM = (instruction[3] ==1 && instruction[15] == 1);
+  
+    // Program counter
+    assign po = ~zr & ~ng;
+    assign lezr = instruction[2] & ng;
+    assign eqzr = instruction[1] & zr;
+    assign gtzr = instruction[0] & po;
+    assign jump = lezr | eqzr | gtzr;
+    assign do_jump = jmp & instruction[15];
+    PC #(15) pc(.in(a_out), .load(do_jump), .inc(~do_jump), .reset(reset), 
+                .clk(clk), .out(pc));
 
-  // A-Register
-  assign a_load = (instruction[15] == 0) ? 1'b1: 1'b0;
-  ARegister #(16) a_register(.in(a_instruction), .load(a_load), .out(a_out));
-  assign aluA = (instruction[12] == 0) ? a_out: inM;
-
-  // D-Register
-  DRegister #(16) d_register(.in(aluOut), .load(instruction[4]), .out(d_out));
-
-  // ALU and output 
-  ALU alu(.x(aluA), .y(d_out), .zx(instruction[11]), .nx(instruction[10]), 
-          .zy(instruction[9]), .ny(instruction[8]), .f(instruction[7]), 
-          .no(instruction[6]), .out(aluOut), .zr(zr), .ng(ng));
-
-  // Memory output
-  assign writeM = (instruction[3] ==1 && instruction[15] == 1);
-
-  // Program counter
-  assign po = ~zr & ~ng;
-  // Jump condition
-  assign lezr = instruction[2] & ng;
-  assign eqzr = instruction[1] & zr;
-  assign gtzr = instruction[0] & po;
-  assign jump = lezr | eqzr | gtzr;
-  assign do_jump = jmp & instruction[15];
-  PC #(15) pc(.in(a_out), .load(do_jump), .inc(~do_jump), .reset(reset), 
-              .out(pc));
+    end
 
   endmodule
 
