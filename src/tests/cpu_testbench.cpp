ce139c355d96dc5017e5962e3cfe11296e1fcf79
CgKaminski <cameron.g.kaminski@gmail.com>
2023-07-16 21:41:06 -0700

    Added tests cpu test bench #13-24

--- src/tests/cpu_testbench.cpp
+++ src/tests/cpu_testbench.cpp
@@ -290,7 +290,298 @@ int main(int argc, char** argv) {
       std::cout << "D: " << std::hex << top->dRegister << std::endl;      
       return 1;
     }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #13: @999
+    std::cout << "Test #13 @999" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0x03e7;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x03e7 || top -> dRegister != 0xffff 
+        || top -> pc != 0x000f || top -> outM != 0x0001
+        || top -> addressM != 0x03e7)
+    {
+      std::cout << "Test #13 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;      
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #14: A = M + 1
+    // inM = 11111
+    std::cout << "Test #14 A = M + 1" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xfde0;
+    top -> reset = 0x0000;
+    top -> inM = 0x2b67;
+    top -> eval();
+    if (top -> aRegister != 0x2b68 || top -> dRegister != 0xffff
+        || top -> pc != 0x0010 || top -> outM != 0x2b68
+        || top -> addressM != 0x2b68)
+    {
+      std::cout << "Test #14 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #15: A M = D
+    std::cout << "Test #15 A M = D" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xe328;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0xffff || top -> dRegister != 0xffff
+        || top -> pc != 0x0011 || top -> outM != 0xffff
+        || top -> addressM != 0x7fff)
+    {
+      std::cout << "Test #15 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #16: @21
+    std::cout << "Test #16 @21" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0x015;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x0015 || top -> dRegister != 0xffff
+        || top -> pc != 0x0012 || top -> outM != 0x0015
+        || top -> addressM != 0x0015)
+    {
+      std::cout << "Test #16 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #17: D + 1;jeq
+    std::cout << "Test #17 D + 1;jeq" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xe7c2;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x0015 || top -> dRegister != 0xffff
+        || top -> pc != 0x0015 || top -> outM != 0x0000
+        || top -> addressM != 0x0015)
+    {
+      std::cout << "Test #17 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #18: @2
+    std::cout << "Test #18 @2" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0x002;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x0002 || top -> dRegister != 0xffff
+        || top -> pc != 0x0016 || top -> outM != 0x0002
+        || top -> addressM != 0x0002)
+    {
+      std::cout << "Test #18 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
     
+    // Test #19: AMD = D + A
+    std::cout << "Test #19 AMD = D + A" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xe0b8;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x0001 || top -> dRegister != 0x0001
+        || top -> pc != 0x0017 || top -> outM != 0x0002
+        || top -> addressM != 0x0001)
+    {
+      std::cout << "Test #19 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #20: M = M + 1
+    // inM = 11111
+    std::cout << "Test #20 M = M + 1" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xfdc8;
+    top -> reset = 0x0000;
+    top -> inM = 0x2b67;
+    top -> eval();
+    if (top -> aRegister != 0x0001 || top -> dRegister != 0x0001
+        || top -> pc != 0x0018 || top -> outM != 0x2b68
+        || top -> addressM != 0x0001)
+    {
+      std::cout << "Test #20 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #21: AM = M - 1
+    // inM = 11111
+    std::cout << "Test #21 AM = M - 1" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xfca8;
+    top -> reset = 0x0000;
+    top -> inM = 0x2b67;
+    top -> eval();
+    if (top -> aRegister != 0x2b66 || top -> dRegister != 0x0001
+        || top -> pc != 0x0019 || top -> outM != 0x2b66
+        || top -> addressM != 0x2b66)
+    {
+      std::cout << "Test #21 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #22: @1000
+    std::cout << "Test #22 @1000" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0x03e8;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x03e8 || top -> dRegister != 0x0001
+        || top -> pc != 0x001a || top -> outM != 0xffff
+        || top -> addressM != 0x03e8)
+    {
+      std::cout << "Test #22 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #23: D = -1
+    std::cout << "Test #23 D = -1" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xee90;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x03e8 || top -> dRegister != 0xffff
+        || top -> pc != 0x001b || top -> outM != 0xffff
+        || top -> addressM != 0x03e8)
+    {
+      std::cout << "Test #23 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
+    top -> clk = !top -> clk;
+    top -> eval();
+
+    // Test #24: D;JGT
+    std::cout << "Test #24 D;JGT" << std::endl;
+    top -> clk = !top -> clk;
+    top -> instruction = 0xe301;
+    top -> reset = 0x0000;
+    top -> inM = 0x0000;
+    top -> eval();
+    if (top -> aRegister != 0x03e8 || top -> dRegister != 0xffff
+        || top -> pc != 0x001c || top -> outM != 0xffff
+        || top -> addressM != 0x03e8)
+    {
+      std::cout << "Test #24 failed" << std::endl;
+      std::cout << "outM: " << std::hex << top->outM << std::endl;
+      std::cout << "writeM: " << std::hex << top->writeM << std::endl;
+      std::cout << "addressM: " << std::hex << top->addressM << std::endl;
+      std::cout << "pc: " << std::hex << top->pc << std::endl;
+      std::cout << "A: " << std::hex << top->aRegister << std::endl;
+      std::cout << "D: " << std::hex << top->dRegister << std::endl;
+      return 1;
+    }
+
     delete top;
     std::cout << "All tests passed successfully" << std::endl;
     return 0;
